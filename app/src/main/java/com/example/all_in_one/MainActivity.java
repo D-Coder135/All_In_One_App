35c71c64d9ace3f03fdca6b353b5a916827a8964
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-20 22:44:30 +0530

    add the YoYo.with() method for animation on the object textView

--- app/src/main/java/com/example/all_in_one/MainActivity.java
+++ app/src/main/java/com/example/all_in_one/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button2);
+
+        YoYo.with(Techniques.SlideInLeft).duration(2000).repeat(0).playOn(textView);
     }
 }
