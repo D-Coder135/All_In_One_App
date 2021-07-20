d232a877211ec5e53996fde948fae027298ff5a0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-20 22:51:51 +0530

    initialize the imageView object

--- app/src/main/java/com/example/all_in_one/MainActivity.java
+++ app/src/main/java/com/example/all_in_one/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView3);
         button = findViewById(R.id.button2);
 
 
