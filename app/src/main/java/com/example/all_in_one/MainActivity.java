8faab49dd99641c2064d62c9d819db2a1ff08d67
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-20 23:03:45 +0530

    initialize the object for the Intent class

--- app/src/main/java/com/example/all_in_one/MainActivity.java
+++ app/src/main/java/com/example/all_in_one/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, LoginMenu.class);
             }
         });
     }
