0b594ecebe5f504b5bed53a73ab44b3346d123a3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-20 23:04:09 +0530

    call the finish() method

--- app/src/main/java/com/example/all_in_one/MainActivity.java
+++ app/src/main/java/com/example/all_in_one/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginMenu.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
