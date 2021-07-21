2995e323d74602b5d81a7eabc47a382703948e45
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-21 23:35:50 +0530

    initialize the object textView

--- app/src/main/java/com/example/all_in_one/LoginMenu.java
+++ app/src/main/java/com/example/all_in_one/LoginMenu.java
@@ -14,5 +14,6 @@ public class LoginMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_menu);
+        textView = findViewById(R.id.textView2);
     }
 }
