5438eb3cec89e1051365364f32af4cb4525fb69e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-21 23:35:16 +0530

    define the object for the class Button

--- app/src/main/java/com/example/all_in_one/LoginMenu.java
+++ app/src/main/java/com/example/all_in_one/LoginMenu.java
@@ -3,10 +3,12 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginMenu extends AppCompatActivity {
     TextView textView;
+    Button offlineButton, emailButton, gmailButton, otpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
