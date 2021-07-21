b233b7129021802a521f703a59b029840bc92e8d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-22 00:00:20 +0530

    initialize the object for the Intent class

--- app/src/main/java/com/example/all_in_one/LoginMenu.java
+++ app/src/main/java/com/example/all_in_one/LoginMenu.java
@@ -2,6 +2,7 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,7 +34,7 @@ public class LoginMenu extends AppCompatActivity {
         offlineButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LoginMenu.this, OfflineLoginPage.class);
             }
         });
     }
