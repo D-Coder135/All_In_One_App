ee4c1938d086c096b5edc2e1c0a9e20189a00350
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-24 00:06:02 +0530

    define the objects for the Button class

--- app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
+++ app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
@@ -3,10 +3,12 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class OfflineLoginPage extends AppCompatActivity {
     TextView heading;
+    Button loginButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
