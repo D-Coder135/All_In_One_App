6227417355f804ff4ef0ff59514022bd3a07a005
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-24 00:28:42 +0530

    add some reformatting

--- app/src/main/java/com/example/all_in_one/MainActivity.java
+++ app/src/main/java/com/example/all_in_one/MainActivity.java
@@ -29,13 +29,10 @@ public class MainActivity extends AppCompatActivity {
         YoYo.with(Techniques.SlideInLeft).duration(1000).repeat(0).playOn(textView);
         YoYo.with(Techniques.BounceIn).duration(3000).repeat(0).playOn(button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginMenu.class);
-                startActivity(intent);
-                finish();
-            }
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginMenu.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
