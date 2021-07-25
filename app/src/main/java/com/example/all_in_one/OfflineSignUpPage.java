065aaae66b4dd20407488c1446c78d3f6e17bee5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 11:24:30 +0530

    define the object for required classes

--- app/src/main/java/com/example/all_in_one/OfflineSignUpPage.java
+++ app/src/main/java/com/example/all_in_one/OfflineSignUpPage.java
@@ -3,8 +3,14 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class OfflineSignUpPage extends AppCompatActivity {
+    TextView heading;
+    Button signUpButton, backButton;
+    EditText emailField, passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
