8af3734d8ec32367863889c893fb8abda37e20c6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 11:31:17 +0530

    initialize the objects with their id's

--- app/src/main/java/com/example/all_in_one/OfflineSignUpPage.java
+++ app/src/main/java/com/example/all_in_one/OfflineSignUpPage.java
@@ -3,6 +3,7 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,5 +17,11 @@ public class OfflineSignUpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline_sign_up_page);
+        heading = findViewById(R.id.textView6);
+        emailField = findViewById(R.id.editText3);
+        passwordField = findViewById(R.id.editText4);
+        passwordField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        signUpButton = findViewById(R.id.button22);
+        backButton = findViewById(R.id.button23);
     }
 }
