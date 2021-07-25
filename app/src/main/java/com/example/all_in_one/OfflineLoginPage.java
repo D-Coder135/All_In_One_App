6c0ea6725ad5afe09edceaf6ca6da06019bf71b2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 10:41:51 +0530

    initialize a String field with the sql query

--- app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
+++ app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
@@ -51,7 +51,7 @@ public class OfflineLoginPage extends AppCompatActivity {
                 Toast.makeText(this, "Please fill the mandatory fields!", Toast.LENGTH_SHORT).show();
             } else {
                 SQLiteDatabase sqLiteDatabase = openOrCreateDatabase("authentication", MODE_PRIVATE, null);
-
+                String data = "select * from user where email ='"+email+"' and password = '"+password+"'";
             }
         });
     }
