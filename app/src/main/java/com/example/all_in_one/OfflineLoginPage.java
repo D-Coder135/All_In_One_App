7609c187411092fc749286e52df16c0bb66ddcee
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 10:47:43 +0530

    initialize an object for the class Intent

--- app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
+++ app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
@@ -57,7 +57,7 @@ public class OfflineLoginPage extends AppCompatActivity {
 
                 if (cursor.getCount() > 0) {
                     Toast.makeText(this, "Logged In Successfully!", Toast.LENGTH_SHORT).show();
-
+                    Intent intent = new Intent(OfflineLoginPage.this, DatabasePage.class);
                 }
             }
         });
