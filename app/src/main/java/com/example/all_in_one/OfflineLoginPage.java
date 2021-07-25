817da39f2415b127a6442d2b79fad453e516541f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-25 10:18:59 +0530

    call the setOnClickListener() method along with the object signUpButton

--- app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
+++ app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
@@ -2,8 +2,10 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -33,7 +35,9 @@ public class OfflineLoginPage extends AppCompatActivity {
         YoYo.with(Techniques.FlipInX).duration(3000).repeat(0).playOn(loginButton);
         YoYo.with(Techniques.FlipInX).duration(3000).repeat(0).playOn(signUpButton);
 
+        signUpButton.setOnClickListener(v -> {
 
+        });
 
     }
 }
