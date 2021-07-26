a4ac18626417e57a7c3c940c097535fc944ed0d2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-26 10:59:27 +0530

    define the object for the Intent class

--- app/src/main/java/com/example/all_in_one/EmailLoginPage.java
+++ app/src/main/java/com/example/all_in_one/EmailLoginPage.java
@@ -2,6 +2,7 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.widget.Button;
@@ -40,7 +41,7 @@ public class EmailLoginPage extends AppCompatActivity {
         YoYo.with(Techniques.FlipInX).duration(3000).repeat(0).playOn(signUpButton);
 
         signUpButton.setOnClickListener(v -> {
-
+            Intent intent = new Intent(EmailLoginPage.this, EmailSignUpPage.class);
         });
     }
 }
