980f17b6f1d3ce3c5c97f034ba0a48df734e70a5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-26 19:32:22 +0530

    define the object for the GoogleSignInClient class

--- app/src/main/java/com/example/all_in_one/GmailLoginPage.java
+++ app/src/main/java/com/example/all_in_one/GmailLoginPage.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.SigningInfo;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class GmailLoginPage extends AppCompatActivity {
     SignInButton signInButton;
     FirebaseAuth firebaseAuth;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
