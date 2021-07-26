ef577711e978e148289acfad678b34293bfa53de
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-26 19:31:50 +0530

    define the object for the SignInButton class

--- app/src/main/java/com/example/all_in_one/GmailLoginPage.java
+++ app/src/main/java/com/example/all_in_one/GmailLoginPage.java
@@ -2,9 +2,13 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.SigningInfo;
 import android.os.Bundle;
 
+import com.google.android.gms.common.SignInButton;
+
 public class GmailLoginPage extends AppCompatActivity {
+    SignInButton signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
