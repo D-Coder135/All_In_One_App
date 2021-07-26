607bd236761a6c94cf0a40d3df4cf34d62d7893e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-26 19:32:06 +0530

    define the object for the FirebaseAuth class

--- app/src/main/java/com/example/all_in_one/GmailLoginPage.java
+++ app/src/main/java/com/example/all_in_one/GmailLoginPage.java
@@ -6,9 +6,11 @@ import android.content.pm.SigningInfo;
 import android.os.Bundle;
 
 import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class GmailLoginPage extends AppCompatActivity {
     SignInButton signInButton;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
