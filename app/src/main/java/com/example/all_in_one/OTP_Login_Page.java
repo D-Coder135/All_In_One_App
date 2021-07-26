eda6972f62a5f2af84ab7f4a096eeb65bd954307
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-26 22:36:37 +0530

    define the object for the class TextView

--- app/src/main/java/com/example/all_in_one/OTP_Login_Page.java
+++ app/src/main/java/com/example/all_in_one/OTP_Login_Page.java
@@ -3,11 +3,13 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.hbb20.CountryCodePicker;
 
 public class OTP_Login_Page extends AppCompatActivity {
     CountryCodePicker countryCodePicker;
+    TextView heading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
