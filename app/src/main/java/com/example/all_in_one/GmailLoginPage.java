1002ff383c7560870f7e562ec4fcc4bada7c8cfa
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-26 19:34:59 +0530

    initialize the object for the class GoogleSignInOptions

--- app/src/main/java/com/example/all_in_one/GmailLoginPage.java
+++ app/src/main/java/com/example/all_in_one/GmailLoginPage.java
@@ -6,6 +6,7 @@ import android.content.pm.SigningInfo;
 import android.os.Bundle;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -20,5 +21,7 @@ public class GmailLoginPage extends AppCompatActivity {
         setContentView(R.layout.activity_gmail_login_page);
         signInButton = findViewById(R.id.signIn);
         firebaseAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("193484657689-s84t6u8n12blifecfb9qih44p7g72o6q.apps.googleusercontent.com").requestEmail().build();
     }
 }
