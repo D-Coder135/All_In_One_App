90b7014cc7c67da2bac720cbdc18da972c5b0a97
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-26 19:12:56 +0530

    add an empty activity GmailLoginPage to the project

---
+++ app/src/main/java/com/example/all_in_one/GmailLoginPage.java
@@ -0,0 +1,14 @@
+package com.example.all_in_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GmailLoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gmail_login_page);
+    }
+}
