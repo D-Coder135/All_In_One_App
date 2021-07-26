eef5eda63a3830d3b8feba1af0fd00736f0c8795
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-26 11:08:56 +0530

    call the setVisibility() method along with the object progressBar

--- app/src/main/java/com/example/all_in_one/EmailLoginPage.java
+++ app/src/main/java/com/example/all_in_one/EmailLoginPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -57,6 +58,7 @@ public class EmailLoginPage extends AppCompatActivity {
                     passwordField.setError("Password Field Can't Be Empty!");
                 }
             }
+            progressBar.setVisibility(View.VISIBLE);
         });
     }
 }
