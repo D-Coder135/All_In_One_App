1de7289d09dc31987258872c9ab7b35b36ea7b45
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 22:57:36 +0530

    add an empty activity ProximityPage to the project

---
+++ app/src/main/java/com/example/all_in_one/ProximityPage.java
@@ -0,0 +1,14 @@
+package com.example.all_in_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProximityPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proximity_page);
+    }
+}
