1c78ca6921f5de90b784ab02b22f5cfb791d368e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 22:51:41 +0530

    define the objects for the Button palette

--- app/src/main/java/com/example/all_in_one/SensorPage.java
+++ app/src/main/java/com/example/all_in_one/SensorPage.java
@@ -3,10 +3,12 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SensorPage extends AppCompatActivity {
     TextView heading;
+    Button proximityButton, accelerometerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
