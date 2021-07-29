e08b6cd9575c294fc818d3e7a2ba8217301e0fc4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 22:51:18 +0530

    define the object for the TextView palette

--- app/src/main/java/com/example/all_in_one/SensorPage.java
+++ app/src/main/java/com/example/all_in_one/SensorPage.java
@@ -3,8 +3,10 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SensorPage extends AppCompatActivity {
+    TextView heading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
