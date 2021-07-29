c6389a0e393ba5e998dcb2444e5e2526467a23f3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 22:33:05 +0530

    add an empty activity SensorPage inside the project

---
+++ app/src/main/java/com/example/all_in_one/SensorPage.java
@@ -0,0 +1,14 @@
+package com.example.all_in_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SensorPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_page);
+    }
+}
