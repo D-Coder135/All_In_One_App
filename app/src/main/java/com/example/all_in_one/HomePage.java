caff5e83c163941f52370a4604f4d45f55c0c131
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 22:12:42 +0530

    define the objects for the Button class

--- app/src/main/java/com/example/all_in_one/HomePage.java
+++ app/src/main/java/com/example/all_in_one/HomePage.java
@@ -3,10 +3,12 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
     TextView heading;
+    Button sensorButton, mediaButton, calculationButton, phoneButton, browserButton, quizButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
