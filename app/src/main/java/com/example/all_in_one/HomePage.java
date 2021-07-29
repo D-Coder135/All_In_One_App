d20d2a3b56ec8f539c961950015c6adb441bc25d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 22:34:06 +0530

    define the object for the Intent class

--- app/src/main/java/com/example/all_in_one/HomePage.java
+++ app/src/main/java/com/example/all_in_one/HomePage.java
@@ -2,6 +2,7 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -34,6 +35,7 @@ public class HomePage extends AppCompatActivity {
         YoYo.with(Techniques.FlipInY).duration(2800).repeat(0).playOn(quizButton);
 
         sensorButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HomePage.this, SensorPage.class);
 
         });
     }
