6e54e05e9a21ec458bde0af6f3d467575cdb02be
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 23:23:10 +0530

    add a else block

--- app/src/main/java/com/example/all_in_one/ProximityBluetooth.java
+++ app/src/main/java/com/example/all_in_one/ProximityBluetooth.java
@@ -28,6 +28,8 @@ public class ProximityBluetooth extends AppCompatActivity implements SensorEvent
     public void onSensorChanged(SensorEvent event) {
         if (event.values[0] > 10) {
             bluetoothAdapter.enable();
+        } else {
+
         }
     }
 
