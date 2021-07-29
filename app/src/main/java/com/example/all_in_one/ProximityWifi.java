1afa5a9d89e085341e5370d063418a10f6dd8fd5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 23:55:23 +0530

    implement the SensorEventListener interface

--- app/src/main/java/com/example/all_in_one/ProximityWifi.java
+++ app/src/main/java/com/example/all_in_one/ProximityWifi.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class ProximityWifi extends AppCompatActivity {
+public class ProximityWifi extends AppCompatActivity implements SensorEventListener {
     SensorManager sensorManager;
     Sensor sensor;
     WifiManager wifiManager;
