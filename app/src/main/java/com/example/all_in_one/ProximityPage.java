9221cbde91e528d3283d9afbb07c6ac5ae28b851
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 23:26:56 +0530

    define the object for the Intent class

--- app/src/main/java/com/example/all_in_one/ProximityPage.java
+++ app/src/main/java/com/example/all_in_one/ProximityPage.java
@@ -2,6 +2,7 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -24,7 +25,7 @@ public class ProximityPage extends AppCompatActivity {
         YoYo.with(Techniques.FlipInY).duration(2800).repeat(0).playOn(flashButton);
 
         bluetoothButton.setOnClickListener(v -> {
-
+            Intent intent = new Intent(ProximityPage.this, ProximityBluetooth.class);
         });
     }
 }
