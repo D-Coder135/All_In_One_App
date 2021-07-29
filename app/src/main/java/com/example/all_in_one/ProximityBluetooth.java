aa9088661e4eda6d6d041ea72354e297bc4d1fe6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 23:18:07 +0530

    define the object for different classes

--- app/src/main/java/com/example/all_in_one/ProximityBluetooth.java
+++ app/src/main/java/com/example/all_in_one/ProximityBluetooth.java
@@ -2,9 +2,15 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 public class ProximityBluetooth extends AppCompatActivity {
+    SensorManager sensorManager;
+    Sensor sensor;
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
