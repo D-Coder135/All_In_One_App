a1c8ceb649a87dd69a8c4b2a3e36d8cbac2fc723
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 00:01:11 +0530

    define the objects for various class

--- app/src/main/java/com/example/all_in_one/ProximityFlash.java
+++ app/src/main/java/com/example/all_in_one/ProximityFlash.java
@@ -1,10 +1,18 @@
 package com.example.all_in_one;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ProximityFlash extends AppCompatActivity {
+    SensorManager sensorManager;
+    Sensor sensor;
+    CameraManager cameraManager;
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
