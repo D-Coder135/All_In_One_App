04680100c56a537eb175d8bcb4c535df7d92023b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 23:53:52 +0530

    define the objects for different class

--- app/src/main/java/com/example/all_in_one/ProximityWifi.java
+++ app/src/main/java/com/example/all_in_one/ProximityWifi.java
@@ -2,9 +2,18 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ProximityWifi extends AppCompatActivity {
+    SensorManager sensorManager;
+    Sensor sensor;
+    WifiManager wifiManager;
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
