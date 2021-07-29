4269efbf45135b9355443c1286add3b85a0ae00e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 23:09:31 +0530

    initialize the object flashButton

--- app/src/main/java/com/example/all_in_one/ProximityPage.java
+++ app/src/main/java/com/example/all_in_one/ProximityPage.java
@@ -14,6 +14,7 @@ public class ProximityPage extends AppCompatActivity {
         setContentView(R.layout.activity_proximity_page);
         bluetoothButton = findViewById(R.id.button60);
         wifiButton = findViewById(R.id.button61);
+        flashButton = findViewById(R.id.button62);
 
     }
 }
