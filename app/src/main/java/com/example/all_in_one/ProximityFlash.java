22b1b8850f4f118b0a95a44162870166df9c9d9a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 00:02:43 +0530

    initialize the objects

--- app/src/main/java/com/example/all_in_one/ProximityFlash.java
+++ app/src/main/java/com/example/all_in_one/ProximityFlash.java
@@ -3,9 +3,12 @@ package com.example.all_in_one;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.camera2.CameraManager;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProximityFlash extends AppCompatActivity {
@@ -14,9 +17,15 @@ public class ProximityFlash extends AppCompatActivity {
     CameraManager cameraManager;
     Button backButton;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proximity_flash);
+        backButton = findViewById(R.id.button67);
+        cameraManager = (CameraManager)getSystemService(CAMERA_SERVICE);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
