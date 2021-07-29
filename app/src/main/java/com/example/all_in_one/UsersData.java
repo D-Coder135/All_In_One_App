668c884a8e4294211eb4c8c319560139f69a2ece
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-29 15:49:47 +0530

    add getter and setter methods for the global fields

--- app/src/main/java/com/example/all_in_one/UsersData.java
+++ app/src/main/java/com/example/all_in_one/UsersData.java
@@ -13,4 +13,36 @@ public class UsersData {
         this.password = password;
         this.mobileNumber = mobileNumber;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 }
