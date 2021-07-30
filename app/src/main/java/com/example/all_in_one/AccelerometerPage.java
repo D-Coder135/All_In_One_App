9c60e7950afc99483f3b69bb027230974f6d17e4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 22:26:40 +0530

    initialize the objects

--- app/src/main/java/com/example/all_in_one/AccelerometerPage.java
+++ app/src/main/java/com/example/all_in_one/AccelerometerPage.java
@@ -12,5 +12,8 @@ public class AccelerometerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer_page);
+        bluetoothButton = findViewById(R.id.button90);
+        wifiButton = findViewById(R.id.button91);
+        flashButton = findViewById(R.id.button92);
     }
 }
