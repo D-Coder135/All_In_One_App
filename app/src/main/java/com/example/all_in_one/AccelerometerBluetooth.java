44d2687d299e10dcd424e55e0df19d1ecf804930
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 22:32:52 +0530

    define the objects for various required classes

--- app/src/main/java/com/example/all_in_one/AccelerometerBluetooth.java
+++ app/src/main/java/com/example/all_in_one/AccelerometerBluetooth.java
@@ -2,11 +2,17 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class AccelerometerBluetooth extends AppCompatActivity {
     Button backButton;
+    SensorManager sensorManager;
+    Sensor sensor;
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
