4f91d5eda4cc0831376c4bdca2d00a677932b7f5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 22:41:00 +0530

    initialize backButton object

--- app/src/main/java/com/example/all_in_one/AccelerometerWifi.java
+++ app/src/main/java/com/example/all_in_one/AccelerometerWifi.java
@@ -18,5 +18,6 @@ public class AccelerometerWifi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer_wifi);
+        backButton = findViewById(R.id.button231);
     }
 }
