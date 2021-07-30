7612a1d087a02c9bc694393150ee1f70c6c1d174
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 22:40:27 +0530

    define the objects for various classes

--- app/src/main/java/com/example/all_in_one/AccelerometerWifi.java
+++ app/src/main/java/com/example/all_in_one/AccelerometerWifi.java
@@ -2,9 +2,17 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AccelerometerWifi extends AppCompatActivity {
+    Button backButton;
+    SensorManager sensorManager;
+    Sensor sensor;
+    WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
