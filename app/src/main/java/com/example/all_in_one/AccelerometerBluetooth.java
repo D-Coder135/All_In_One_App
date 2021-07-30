abf346c3add98b75fff8a265423cbd4387351168
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 22:32:21 +0530

    define the object for the Button class

--- app/src/main/java/com/example/all_in_one/AccelerometerBluetooth.java
+++ app/src/main/java/com/example/all_in_one/AccelerometerBluetooth.java
@@ -3,8 +3,10 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AccelerometerBluetooth extends AppCompatActivity {
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
