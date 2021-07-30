404facb2df3567e9f428d04e2bc413ddf9353ff2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 22:50:08 +0530

    define the object for various classes

--- app/src/main/java/com/example/all_in_one/AccelerometerFlash.java
+++ app/src/main/java/com/example/all_in_one/AccelerometerFlash.java
@@ -2,9 +2,18 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AccelerometerFlash extends AppCompatActivity {
+    Button backButton;
+    SensorManager sensorManager;
+    Sensor sensor;
+    CameraManager cameraManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
