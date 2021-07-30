45924760b6dd510259d2be76de6ca1667fa63009
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-30 22:25:21 +0530

    call the setOnClickListener() method along with the object accelerometerButton

--- app/src/main/java/com/example/all_in_one/SensorPage.java
+++ app/src/main/java/com/example/all_in_one/SensorPage.java
@@ -31,5 +31,9 @@ public class SensorPage extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
+
+        accelerometerButton.setOnClickListener(v -> {
+
+        });
     }
 }
