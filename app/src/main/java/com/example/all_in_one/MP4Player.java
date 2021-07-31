a08b7574d0b47d68c658ca3f63ac8627fe28e2d5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-31 21:22:53 +0530

    define the objects for the MediaController class

--- app/src/main/java/com/example/all_in_one/MP4Player.java
+++ app/src/main/java/com/example/all_in_one/MP4Player.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MP4Player extends AppCompatActivity {
     Button playButton, stopButton, backButton;
     VideoView videoView;
+    MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
