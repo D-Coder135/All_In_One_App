e7948c2b61b4a3da66f290980583c85b9072200c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-31 21:31:59 +0530

    call the setOnClickListener() method along with the object backButton

--- app/src/main/java/com/example/all_in_one/MP4Player.java
+++ app/src/main/java/com/example/all_in_one/MP4Player.java
@@ -2,6 +2,7 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.MediaController;
@@ -30,5 +31,11 @@ public class MP4Player extends AppCompatActivity {
         });
 
         stopButton.setOnClickListener(v -> videoView.stopPlayback());
+
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MP4Player.this, MediaPage.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
