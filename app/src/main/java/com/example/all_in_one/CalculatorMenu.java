64a164908e5261722f3f63cb6a136736f42b47ba
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-31 22:23:39 +0530

    define the objects for the Button class

--- app/src/main/java/com/example/all_in_one/CalculatorMenu.java
+++ app/src/main/java/com/example/all_in_one/CalculatorMenu.java
@@ -3,8 +3,10 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CalculatorMenu extends AppCompatActivity {
+    Button simpleCalcButton, calcWithTTTButton, backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
