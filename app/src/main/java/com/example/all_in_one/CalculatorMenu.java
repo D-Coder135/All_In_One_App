7006d270e83f83768652adfdfd736a3754c126dd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 00:01:55 +0530

    call the YoYo.with() button on different objects

--- app/src/main/java/com/example/all_in_one/CalculatorMenu.java
+++ app/src/main/java/com/example/all_in_one/CalculatorMenu.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 public class CalculatorMenu extends AppCompatActivity {
     TextView heading;
     Button simpleCalcButton, calcWithTTTButton, backButton;
@@ -19,5 +22,10 @@ public class CalculatorMenu extends AppCompatActivity {
         simpleCalcButton = findViewById(R.id.button163);
         calcWithTTTButton = findViewById(R.id.button164);
         backButton = findViewById(R.id.button165);
+
+        YoYo.with(Techniques.FlipInX).duration(1500).repeat(0).playOn(heading);
+        YoYo.with(Techniques.FlipInY).duration(2800).repeat(0).playOn(simpleCalcButton);
+        YoYo.with(Techniques.FlipInY).duration(2800).repeat(0).playOn(calcWithTTTButton);
+        YoYo.with(Techniques.FlipInY).duration(2800).repeat(0).playOn(backButton);
     }
 }
