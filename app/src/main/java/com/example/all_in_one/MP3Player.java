a5916e40fc3a770f73b76346373499ec9c08d716
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-31 19:17:51 +0530

    add an empty activity MP3Player to the project

---
+++ app/src/main/java/com/example/all_in_one/MP3Player.java
@@ -0,0 +1,14 @@
+package com.example.all_in_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MP3Player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mp3_player);
+    }
+}
