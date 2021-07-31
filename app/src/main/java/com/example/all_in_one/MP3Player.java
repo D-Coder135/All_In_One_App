07a4ebff7a9c890c2f3db9f8522f0aea7ef1b024
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-31 19:45:33 +0530

    initialize the object playButton

--- app/src/main/java/com/example/all_in_one/MP3Player.java
+++ app/src/main/java/com/example/all_in_one/MP3Player.java
@@ -14,5 +14,6 @@ public class MP3Player extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mp3_player);
+        playButton = findViewById(R.id.button140);
     }
 }
