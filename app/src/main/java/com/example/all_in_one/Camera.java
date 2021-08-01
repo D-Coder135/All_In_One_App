e17064debb9e853c22c5c576cc62e7ea2ab6306a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 23:42:25 +0530

    initialize the object imageView

--- app/src/main/java/com/example/all_in_one/Camera.java
+++ app/src/main/java/com/example/all_in_one/Camera.java
@@ -14,5 +14,6 @@ public class Camera extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        imageView = findViewById(R.id.imageView);
     }
 }
