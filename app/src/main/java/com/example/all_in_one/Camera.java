41f20644bb4a6195d163416ac0a99bf9c982850d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 23:40:48 +0530

    define the object for the Button class

--- app/src/main/java/com/example/all_in_one/Camera.java
+++ app/src/main/java/com/example/all_in_one/Camera.java
@@ -3,10 +3,12 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Camera extends AppCompatActivity {
     ImageView imageView;
+    Button cameraButton, backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
