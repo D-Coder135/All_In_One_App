7be3104c371a9a89c39226b3383b847803acfd5f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 15:54:13 +0530

    define the objects for the Button class

--- app/src/main/java/com/example/all_in_one/SimpleCalc.java
+++ app/src/main/java/com/example/all_in_one/SimpleCalc.java
@@ -3,10 +3,12 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class SimpleCalc extends AppCompatActivity {
     EditText firstNumberField, secondNumberField;
+    Button add, sub, mul, div, backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
