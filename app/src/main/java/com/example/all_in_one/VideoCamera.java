de04dbbbfc78b76c1c0ccbb273db94a18089d4b8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 23:56:36 +0530

    initialize the object videoView

--- app/src/main/java/com/example/all_in_one/VideoCamera.java
+++ app/src/main/java/com/example/all_in_one/VideoCamera.java
@@ -14,5 +14,6 @@ public class VideoCamera extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_camera);
+        videoView = findViewById(R.id.videoView2);
     }
 }
