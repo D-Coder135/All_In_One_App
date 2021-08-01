a969b96682ca3b1876022be7755eefe3cdadf761
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 23:57:00 +0530

    define the objects for the class MediaController and Uri

--- app/src/main/java/com/example/all_in_one/VideoCamera.java
+++ app/src/main/java/com/example/all_in_one/VideoCamera.java
@@ -1,7 +1,9 @@
 package com.example.all_in_one;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class VideoCamera extends AppCompatActivity {
     VideoView videoView;
     Button cameraButton, backButton;
+    MediaController mediaController;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
