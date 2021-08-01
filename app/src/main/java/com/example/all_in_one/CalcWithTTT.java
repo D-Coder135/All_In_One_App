1f23df1cc571fcdd87bfb60a4883261f1716795e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 16:31:36 +0530

    add some code along with some fix up

--- app/src/main/java/com/example/all_in_one/CalcWithTTT.java
+++ app/src/main/java/com/example/all_in_one/CalcWithTTT.java
@@ -8,6 +8,7 @@ import android.speech.tts.TextToSpeech;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
@@ -50,6 +51,54 @@ public class CalcWithTTT extends AppCompatActivity {
             Intent intent = new Intent(CalcWithTTT.this, CalculatorMenu.class);
             startActivity(intent);
             finish();
+        });
+
+        add.setOnClickListener(v -> {
+            String firstNumber = firstNumberField.getText().toString();
+            String secondNumber = secondNumberField.getText().toString();
+            Float firsNumberInFloat = Float.parseFloat(firstNumber);
+            Float secondNumberInFloat = Float.parseFloat(secondNumber);
+            float resultInFloat = firsNumberInFloat + secondNumberInFloat;
+            String resultInString = Float.toString(resultInFloat);
+            resultView.setText("Final Result = " +resultInString);
+            textToSpeech.speak(resultInString, TextToSpeech.QUEUE_FLUSH, null);
+            Toast.makeText(CalcWithTTT.this, "The result is = " + resultInString, Toast.LENGTH_SHORT).show();
+        });
+
+        sub.setOnClickListener(v -> {
+            String firstNumber = firstNumberField.getText().toString();
+            String secondNumber = secondNumberField.getText().toString();
+            Float firsNumberInFloat = Float.parseFloat(firstNumber);
+            Float secondNumberInFloat = Float.parseFloat(secondNumber);
+            float resultInFloat = firsNumberInFloat - secondNumberInFloat;
+            String resultInString = Float.toString(resultInFloat);
+            resultView.setText("Final Result = " +resultInString);
+            textToSpeech.speak(resultInString, TextToSpeech.QUEUE_FLUSH, null);
+            Toast.makeText(CalcWithTTT.this, "The result is = " + resultInString, Toast.LENGTH_SHORT).show();
+        });
+
+        mul.setOnClickListener(v -> {
+            String firstNumber = firstNumberField.getText().toString();
+            String secondNumber = secondNumberField.getText().toString();
+            Float firsNumberInFloat = Float.parseFloat(firstNumber);
+            Float secondNumberInFloat = Float.parseFloat(secondNumber);
+            float resultInFloat = firsNumberInFloat * secondNumberInFloat;
+            String resultInString = Float.toString(resultInFloat);
+            resultView.setText("Final Result = " +resultInString);
+            textToSpeech.speak(resultInString, TextToSpeech.QUEUE_FLUSH, null);
+            Toast.makeText(CalcWithTTT.this, "The result is = " + resultInString, Toast.LENGTH_SHORT).show();
+        });
+
+        div.setOnClickListener(v -> {
+            String firstNumber = firstNumberField.getText().toString();
+            String secondNumber = secondNumberField.getText().toString();
+            Float firsNumberInFloat = Float.parseFloat(firstNumber);
+            Float secondNumberInFloat = Float.parseFloat(secondNumber);
+            float resultInFloat = firsNumberInFloat / secondNumberInFloat;
+            String resultInString = Float.toString(resultInFloat);
+            resultView.setText("Final Result = " +resultInString);
+            textToSpeech.speak(resultInString, TextToSpeech.QUEUE_FLUSH, null);
+            Toast.makeText(CalcWithTTT.this, "The result is = " + resultInString, Toast.LENGTH_SHORT).show();
         });
     }
 }
