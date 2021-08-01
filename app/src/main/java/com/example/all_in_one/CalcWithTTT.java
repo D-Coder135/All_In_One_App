06b5dd04b5ec612b0183c80a1c2449478f5acb6e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 16:26:55 +0530

    define the objects for various classes

--- app/src/main/java/com/example/all_in_one/CalcWithTTT.java
+++ app/src/main/java/com/example/all_in_one/CalcWithTTT.java
@@ -3,8 +3,14 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalcWithTTT extends AppCompatActivity {
+    EditText firstNumberField, secondNumberField;
+    Button add, sub, mul, div, backButton;
+    TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
