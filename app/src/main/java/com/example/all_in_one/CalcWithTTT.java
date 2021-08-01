ebfb3624627dc9777a8720c6915dc2a70747b4c4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 16:27:16 +0530

    define the object for the class TextToSpeech

--- app/src/main/java/com/example/all_in_one/CalcWithTTT.java
+++ app/src/main/java/com/example/all_in_one/CalcWithTTT.java
@@ -3,6 +3,7 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ public class CalcWithTTT extends AppCompatActivity {
     EditText firstNumberField, secondNumberField;
     Button add, sub, mul, div, backButton;
     TextView resultView;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
