e8f7740e33cd675411e4cf63de0372fc5be542de
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-01 20:54:13 +0530

    initialize the object for the class Intent

--- app/src/main/java/com/example/all_in_one/CameraMenu.java
+++ app/src/main/java/com/example/all_in_one/CameraMenu.java
@@ -2,6 +2,7 @@ package com.example.all_in_one;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -28,7 +29,9 @@ public class CameraMenu extends AppCompatActivity {
         YoYo.with(Techniques.FlipInY).duration(2800).repeat(0).playOn(backButton);
 
         backButton.setOnClickListener(v -> {
-
+            Intent intent = new Intent(CameraMenu.this, HomePage.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
