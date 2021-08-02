485928b1898c6c35cd416ba5c6382c19356fbb7c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-02 14:59:47 +0530

    call the YoYo.with() method

--- app/src/main/java/com/example/all_in_one/QuizPage.java
+++ app/src/main/java/com/example/all_in_one/QuizPage.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 public class QuizPage extends AppCompatActivity {
     Button startButton, backButton;
 
@@ -14,5 +17,8 @@ public class QuizPage extends AppCompatActivity {
         setContentView(R.layout.activity_quiz_page);
         startButton = findViewById(R.id.button850);
         backButton = findViewById(R.id.button851);
+
+        YoYo.with(Techniques.FlipInX).duration(2800).repeat(0).playOn(startButton);
+        YoYo.with(Techniques.FlipInX).duration(2800).repeat(0).playOn(backButton);
     }
 }
