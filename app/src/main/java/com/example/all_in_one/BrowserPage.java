a8098ce684a9f9fe468db34aae9b1861b4b7108a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-02 14:39:09 +0530

    define the object for the EditText class

--- app/src/main/java/com/example/all_in_one/BrowserPage.java
+++ app/src/main/java/com/example/all_in_one/BrowserPage.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.EditText;
 
 public class BrowserPage extends AppCompatActivity {
     WebView webView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
