eea3350537f5206b7e4d93517c23125dc0055a2c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-02 14:38:30 +0530

    define the object for the WebView class

--- app/src/main/java/com/example/all_in_one/BrowserPage.java
+++ app/src/main/java/com/example/all_in_one/BrowserPage.java
@@ -3,8 +3,10 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class BrowserPage extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
