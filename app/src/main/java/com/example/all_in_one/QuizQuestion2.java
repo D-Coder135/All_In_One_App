42139ed1aee3b512a4dc101d0fc473c3f10ecf2d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-02 15:39:46 +0530

    initialize the objects

--- app/src/main/java/com/example/all_in_one/QuizQuestion2.java
+++ app/src/main/java/com/example/all_in_one/QuizQuestion2.java
@@ -10,11 +10,17 @@ import android.widget.TextView;
 public class QuizQuestion2 extends AppCompatActivity {
     TextView question;
     Button nextButton;
-    RadioButton firstOption, secondOption;
+    RadioButton firstOption, secondOption, thirdOption, fourthOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_question2);
+        question = findViewById(R.id.textView131);
+        nextButton = findViewById(R.id.button831);
+        firstOption = findViewById(R.id.radioButton3);
+        secondOption = findViewById(R.id.radioButton4);
+        thirdOption = findViewById(R.id.radioButton5);
+        fourthOption = findViewById(R.id.radioButton6);
     }
 }
