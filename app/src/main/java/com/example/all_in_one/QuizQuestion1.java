d4b591b9aff222f2d88af4f8c2aa3c2b33745bb3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-02 15:17:59 +0530

    define the objects for various classes

--- app/src/main/java/com/example/all_in_one/QuizQuestion1.java
+++ app/src/main/java/com/example/all_in_one/QuizQuestion1.java
@@ -3,9 +3,13 @@ package com.example.all_in_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class QuizQuestion1 extends AppCompatActivity {
     Button nextButton;
+    RadioButton firstOption, secondOption;
+    static int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
