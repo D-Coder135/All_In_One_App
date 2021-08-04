9bf39d5afdf3fb5ca139e67b8b72d9d17eb8cdd5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-04 22:26:46 +0530

    optimize imports

--- app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
+++ app/src/main/java/com/example/all_in_one/OfflineLoginPage.java
@@ -1,17 +1,16 @@
 package com.example.all_in_one;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
